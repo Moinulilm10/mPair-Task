9d8ed5a2c8c7917898b710f26026ba11da99ff87
Moinul Islam <moinulilm10@gmail.com>
2024-05-21 16:02:54 +0600

    "Updated App component to include Navbar and restructured layout; added Navbar component with logo, avatar, and dropdown menu"

--- mPair client/src/App.jsx
+++ mPair client/src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+import NavBar from "./components/shared/Navbar";
+
+const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex flex-col h-screen">
+      <NavBar />
+      <div className="flex flex-1">
+        <h1>Sidebar</h1>
+        <div className="flex-1 p-5">
+          <h1>Profile</h1>
+          <h1>Register</h1>
+          <h1>Login</h1>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
--- mPair client/src/components/shared/Navbar.jsx
+++ mPair client/src/components/shared/Navbar.jsx
@@ -1,5 +1,45 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
+import avatar from "../../assets/avatar.jpg";
+import logo from "../../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div style={{ background: "rgba(35, 151, 200, 1)" }}>
+        <div className="px-10 navbar">
+          <div className="flex-1">
+            <img src={logo} className="h-7"></img>
+          </div>
+          <div className="flex-none gap-2">
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img alt="Tailwind CSS Navbar component" src={avatar} />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">Profile</a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Navbar;
+export default NavBar;
