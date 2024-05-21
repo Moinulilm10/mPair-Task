6d2b050649a1e5694b31f54fce683aeb150e76f7
Moinul Islam <moinulilm10@gmail.com>
2024-05-21 20:43:42 +0600

    "Implemented Login and Register components with form fields and submission handlers"

--- mPair client/src/components/auth/Login.jsx
+++ mPair client/src/components/auth/Login.jsx
@@ -1,5 +1,126 @@
 const Login = () => {
-  return <div>Login</div>;
+  // const [employeeID, setEmployeeID] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [error, setError] = useState("");
+  // const [loading, setLoading] = useState(false);
+
+  // const navigate = useNavigate();
+  // const { login } = useContext(AuthContext);
+
+  // const handleLogin = async (e) => {
+  //   e.preventDefault();
+  //   setLoading(true);
+  //   setError("");
+
+  //   try {
+  //     const response = await fetch("http://localhost:4000/api/user/login", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ eid: employeeID, password }),
+  //     });
+
+  //     const data = await response.json();
+
+  //     if (data.message === "Login Successful !") {
+  //       toast.success(data.message);
+  //       localStorage.setItem("token", data.token);
+  //       login();
+  //       navigate("/");
+  //     } else {
+  //       toast.error(data.message);
+  //     }
+  //   } catch (err) {
+  //     setError("An error occurred during login. Please try again later.");
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  return (
+    <div>
+      <div
+        className="w-[414px] p-10 mx-auto border-blue-400 border-t-4 mt-10"
+        style={{ background: "rgba(244, 250, 252, 1)" }}
+      >
+        <div className="w-[328px]">
+          <div className="mb-8">
+            <h4
+              className="my-2 text-xl font-bold"
+              style={{ color: "rgba(35, 151, 200, 1)" }}
+            >
+              Welcome to PureLedger
+            </h4>
+            <p
+              className="my-2 text-base"
+              style={{ color: "rgba(158, 158, 158, 1)" }}
+            >
+              Please Login to continue
+            </p>
+          </div>
+
+          <form>
+            <div className="py-2">
+              <label htmlFor="eid">
+                Employee ID
+                <input
+                  type="text"
+                  placeholder="Enter Employee ID here"
+                  name="eid"
+                  className="w-full mt-2 rounded-md input input-bordered"
+                  required
+                  // value={employeeID}
+                  // onChange={(e) => setEmployeeID(e.target.value)}
+                />
+              </label>
+            </div>
+
+            <div className="py-2">
+              <label htmlFor="password">
+                Password
+                <input
+                  type="password"
+                  placeholder="Enter Password here"
+                  name="password"
+                  className="w-full rounded-md input input-bordered"
+                  required
+                  // value={password}
+                  // onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+            </div>
+
+            {/* {error && <div className="my-2 text-sm text-red-500">{error}</div>} */}
+
+            <div>
+              <button
+                type="submit"
+                className="w-full h-8 text-white rounded mt-7"
+                style={{ background: "rgba(35, 151, 200, 1)" }}
+                // disabled={loading}
+              >
+                {/* {loading ? "Logging in..." : "Login"} */}
+              </button>
+            </div>
+          </form>
+
+          <div className="flex justify-center gap-2 mt-8 text-sm">
+            <p style={{ color: "rgba(158, 158, 158, 1)" }}>
+              Don’t have an account?
+            </p>
+            <p
+              className="cursor-pointer"
+              style={{ color: "rgba(35, 151, 200, 1)" }}
+              // onClick={() => navigate("/register")}
+            >
+              Register Now!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- mPair client/src/components/auth/Register.jsx
+++ mPair client/src/components/auth/Register.jsx
@@ -1,5 +1,175 @@
 const Register = () => {
-  return <div>Register</div>;
+  // const navigate = useNavigate();
+  // const [formData, setFormData] = useState({
+  //   name: "",
+  //   gender: "",
+  //   dob: "",
+  //   email: "",
+  //   eid: "",
+  //   position: "",
+  //   password: "",
+  // });
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({
+  //     ...formData,
+  //     [name]: value,
+  //   });
+  // };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   try {
+  //     const response = await fetch("http://localhost:4000/api/user/register", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(formData),
+  //     });
+
+  //     if (!response.ok) {
+  //       throw new Error("Network response was not ok");
+  //     }
+
+  //     const data = await response.json();
+  //     if (data.message === "Registered Successfully !!!") {
+  //       toast.dismiss();
+  //       toast.success(data.message);
+  //       navigate("/");
+  //     } else {
+  //       toast.dismiss();
+  //       toast.error(data.message);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error:", error);
+  //   }
+  // };
+
+  return (
+    <div>
+      <div
+        className="h-[609px] w-[414px] p-10 mx-auto border-blue-400 border-t-4 mt-10"
+        style={{ background: "rgba(244, 250, 252, 1)" }}
+      >
+        <div className="h-[510px] w-[328px]">
+          <div className="mb-8">
+            <h4
+              className="my-2 text-xl font-bold"
+              style={{ color: "rgba(35, 151, 200, 1)" }}
+            >
+              Welcome to PureLedger
+            </h4>
+            <p
+              className="my-2 text-base "
+              style={{ color: "rgba(158, 158, 158, 1)" }}
+            >
+              Fill up this form to Register
+            </p>
+          </div>
+          <div>
+            <form>
+              <div className="py-2">
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  name="name"
+                  className="w-full rounded-md input input-bordered"
+                  // value={formData.name}
+                  // onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="flex gap-2">
+                <div className="w-full py-2">
+                  <select
+                    id="gender"
+                    name="gender"
+                    // value={formData.gender}
+                    // onChange={handleChange}
+                    className={`w-full px-2 py-1 border rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500 h-12 text-gray-400`}
+                    required
+                  >
+                    <option value="" disabled>
+                      Gender
+                    </option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+                <div className="py-2">
+                  <input
+                    type="date"
+                    name="dob"
+                    className="w-full text-gray-400 rounded-md input input-bordered"
+                    // value={formData.dob}
+                    // onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="py-2">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  className="w-full rounded-md input input-bordered"
+                  // value={formData.email}
+                  // onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="py-2">
+                <input
+                  type="text"
+                  placeholder="Employee ID"
+                  name="eid"
+                  className="w-full rounded-md input input-bordered"
+                  // value={formData.eid}
+                  // onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="py-2">
+                <input
+                  type="text"
+                  placeholder="Position In Organization"
+                  name="position"
+                  className="w-full rounded-md input input-bordered"
+                  // value={formData.position}
+                  // onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="py-2">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  className="w-full rounded-md input input-bordered"
+                  // value={formData.password}
+                  // onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full h-8 text-white rounded mt-7"
+                  style={{ background: "rgba(35, 151, 200, 1)" }}
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
