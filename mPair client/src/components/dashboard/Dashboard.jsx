ad1416fc30dbf69bfbb6ea3c89cc11ea83bb6d4d
Moinul Islam <moinulilm10@gmail.com>
2024-05-21 17:04:08 +0600

    "Added Chart component to Dashboard"

--- mPair client/src/components/dashboard/Dashboard.jsx
+++ mPair client/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
+import Chart from "./Chart";
 import ExpenseCard from "./ExpenseCard";
 
 const Dashboard = () => {
   return (
     <div className="flex flex-col justify-center">
       <ExpenseCard />
+      <Chart />
     </div>
   );
 };
